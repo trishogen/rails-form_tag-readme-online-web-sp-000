ea503a8ff15c6f182e2e4a01574418cd3f373d05
Trish Ogen <patriciaogen@gmail.com>
2019-10-27 21:39:45 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,5 +7,7 @@ class PostsController < ApplicationController
   end
 
   def create
+    Post.create(title: params[:post][:title], description: params[:post][:description])
+  redirect_to posts_path
   end
 end
